f83f43daeb494a1e45448fb07cc26813ea0cfcee
Fiama Arrúa <fiamaarrua5@gmail.com>
2023-09-08 14:35:20 -0300

    Guardar id de usuario logueado

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/service/auth.service';
 import { Router } from '@angular/router'; //importacion para poder utilizar el routing
+import { User } from 'src/app/model/user';
 
 
 @Component({
@@ -9,7 +10,16 @@ import { Router } from '@angular/router'; //importacion para poder utilizar el r
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  user = { username: '', password: '' };
+  user: User = {
+    id: 0,
+    username: '',
+    name: '',
+    password: '',
+    email: '',
+
+
+
+  }
 
   constructor(private authService: AuthService, private router: Router) { }
 
@@ -17,7 +27,11 @@ export class LoginComponent {
     this.authService.login(this.user).subscribe(
       (response: any) => {
         console.log('Inicio de sesión exitoso:', response.message);
+        const userId = response.userId
+        this.authService.setUserId(userId); //Obtener id del usuario una vez logueado
         this.router.navigate(['/home'])  //redirigir una vez el usuario sea correcto
+        console.log('Id usuario:', userId)
+
       },
       (error) => {
         console.error('Error en el inicio de sesión:', error);
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -7,10 +7,21 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
   private apiUrl = 'http://localhost:8080/auth/login';
+  private userId: number | null = null;
 
   constructor(private http: HttpClient) { }
 
   login(user: { username: string, password: string }): Observable<any> {
     return this.http.post(this.apiUrl, user);
+  }
+
+   // Función para establecer el ID del usuario
+   setUserId(userId: number): void {
+    this.userId = userId;
+  }
+
+  // Función para obtener el ID del usuario
+  getUserId(): number | null {
+    return this.userId;
   }
 }
